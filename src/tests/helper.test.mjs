aa23e9744906bfc836e442eb3eccb50f24263df7
NickCallaghan <nicholas.callaghan@gmail.com>
2023-12-29 15:17:31 +0000

    fix failing test

--- src/tests/helper.test.mjs
+++ src/tests/helper.test.mjs
@@ -2,6 +2,6 @@ import { add_two } from "./helper.mjs";
 
 describe("add_two", () => {
     it("adds two numbers", () => {
-        expect(add_two(1, 2)).toBe(5);
+        expect(add_two(1, 2)).toBe(3);
     });
 });
